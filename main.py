c75c706dc84bc0a7bc54672cafcd0443cf70e322
OmskShip <Ruzhelovich.Vladimir@gmail.com>
2021-10-19 08:59:22 +0600

    изменение сообщения

--- main.py
+++ main.py
@@ -9,22 +9,26 @@ if __name__ == '__main__':
     logger = log.init()
 
     control_list = settings.CONTROL_SUPPLIER
-    SQL = settings.SQL
 
     for control_supplier in control_list:
         logger.info(f'Обработка поставщика: {control_supplier["supplier_name"]}')
+
+        BOT_TOKEN = settings.BOT_TOKEN if 'BOT_TOKEN' not in control_supplier else control_supplier.BOT_TOKEN
+        SQL = settings.SQL if 'SQL' not in control_supplier else control_supplier.SQL
+
         sql_query = SQL.replace('@supplier_id', f'{control_supplier["supplier_id"]}')
+
         control_supplier['sql_query'] = sql_query
 
         orders = Request(control_supplier).run()
 
         # Подготовка сообщения
-        text = f'Поступили новые заказы {control_supplier["supplier_name"]}:'
+        text = f'Новые уведомления по {control_supplier["supplier_name"]}:'
         for inx, order in enumerate(orders):
             text_order = '\n'.join([f'{key.capitalize()}: {value}' for key, value in order.items()])
-            text = f'{text}\n{"*"*10}Заказ №{inx+1}{"*"*10}\n{text_order}'
+            text = f'{text}\n{"*"*10}Уведомление №{inx+1}{"*"*10}\n{text_order}'
 
         # Отправка сообщение в группы телеграмм
-        if text != f'Поступили новые заказы {control_supplier["supplier_name"]}:':
+        if text != f'Новые уведомления по {control_supplier["supplier_name"]}:':
             for chat in control_supplier["chats"]:
-                TelegramClass(settings.BOT_TOKEN).send_to_chat(f'@{chat}', text)
+                TelegramClass(BOT_TOKEN).send_to_chat(f'{chat}', text)
